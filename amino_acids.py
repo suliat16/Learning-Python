943d8cd428b5c8dcff1fb833edb1cf94042651cc
suliat16 <suliat16@gmail.com>
2018-05-15 18:39:33 +0300

    Added all codons into a dictionary

--- amino_acids.py
+++ amino_acids.py
@@ -20,47 +20,110 @@ def codon_to_aa(codon = 'AUG'):
         codon, please try again"
     '''
 
+    ct = dict()
+    ct['AUG']= 'Methionine'
+    ct['UUU']= 'Phenylalanine'
+    ct['UUC']= 'Phenylalanine'
+    ct['UUA']= 'Leucine'
+    ct['UUG']= 'Leucine'
+    ct['CUU']= 'Leucine'
+    ct['CUC']= 'Leucine'
+    ct['CUA']= 'Leucine'
+    ct['CUG']= 'Leucine'
+    ct['AUU']= 'Isoleucine'
+    ct['AUC']= 'Isoleucine'
+    ct['AUA']= 'Isoleucine'
+    ct['GUU']= 'Valine'
+    ct['GUC']= 'Valine'
+    ct['GUA']= 'Valine'
+    ct['GUG']= 'Valine'
+    ct['UCU']= 'Serine'
+    ct['UCC']= 'Serine'
+    ct['UCA']= 'Serine'
+    ct['UCG']= 'Serine'
+    ct['CCU']= 'Proline'
+    ct['CCC']= 'Proline'
+    ct['CCA']= 'Proline'
+    ct['CCG']= 'Proline'
+    ct['ACU']= 'Threonine'
+    ct['ACC']= 'Threonine'
+    ct['ACA']= 'Threonine'
+    ct['ACG']= 'Threonine'
+    ct['GCU']= 'Alanine'
+    ct['GCC']= 'Alanine'
+    ct['GCA']= 'Alanine'
+    ct['GCG']= 'Alanine'
+    ct['UAU']= 'Tyrosine'
+    ct['UAC']= 'Tyrosine'
+    ct['CAU']= 'Histidine'
+    ct['CAC']= 'Histidine'
+    ct['CAA']= 'Glutamine'
+    ct['CAG']= 'Glutamine'
+    ct['AAU']= 'Lysine'
+    ct['AAC']= 'Lysine'
+    ct['GAU']= 'Glutamate'
+    ct['GAC']= 'Glutamate'
+    ct['UGU']= 'Cysteine'
+    ct['UGC']= 'Cysteine'
+    ct['UGG']= 'Tryptophan'
+    ct['CGU']= 'Arginine'
+    ct['CGC']= 'Arginine'
+    ct['CGA']= 'Arginine'
+    ct['CGG']= 'Arginine'
+    ct['AGA']= 'Arginine'
+    ct['AGG']= 'Arginine'
+    ct['AGU']= 'Serine'
+    ct['AGC']= 'Serine'
+    ct['GGU']= 'Glycine'
+    ct['GGC']= 'Glycine'
+    ct['GGA']= 'Glycine'
+    ct['GGG']= 'Glycine'
+    ct['UAA']= 'Stop codon'
+    ct['UAG']= 'Stop codon'
+    ct['UGA'] = 'Stop codon'
+    
+    return ct[codon]
+    
 # =============================================================================
-#     if codon == 'AUG':
-#         return 'Methionine'
-#     elif codon == 'UUU' or 'UUC':
-#         return 'Phenylalanine'
-#     elif codon == 'UUA' or 'UUA' or 'CUU' or 'CUC' or 'CUA' or 'CUG':
-#         return 'Leucine'
-#     elif codon == 'AUU' or 'AUC' or 'AUA':
-#         return 'Isoleucine'
-#     elif codon == 'GUU' or 'GUC' or 'GUA' or 'GUG':
-#         return 'Valine'
-#     elif codon == 'UCU' or 'UCC' or 'UCA' or 'UCG':
-#         return 'Serine'
-#     elif codon ==  'CCU' or 'CCC' or 'CCA' or 'CCG':
-#         return 'Proline'
-#     elif codon == 'ACU' or 'ACC' or 'ACA' or 'ACG':
-#         return 'Threonine'
-#     elif codon == 'GCU' or 'GCC' or 'GCA' or 'GCG':
-#         return 'Alanine'
-#     elif codon == 'UAU' or 'UAC' :
-#         return 'Tyrosine'
-#     elif codon == 'CAU' or 'CAC':
-#         return 'Histidine'
-#     elif codon == 'CAA' or 'CAG':
-#         return 'Glutamine'
-#     elif codon == 'AAU' or 'AAC':
-#         return 'Lysine'
-#     elif codon == 'GAU' or 'GAC':
-#         return 'Glutamate'
-#     elif codon == 'UGU' or 'UGC':
-#         return 'Cysteine'
-#     elif codon == 'UGG':
-#         return 'Tryptophan'
-#     elif codon == 'CGU' or 'CGC' or ' CGA' or 'CGG' or 'AGA' or 'AGG':
-#         return 'Arginine'
-#     elif codon == 'AGU' or 'AGC':
-#         return 'Serine'
-#     elif codon == 'GGU' or 'GGC' or 'GGA' or 'GGG':
-#         return 'Glycine'
-#     else:
-#         return 'Stop codon'
+# #     if codon == 'AUG':
+# #         return 'Methionine'
+# #  #   elif codon == 'UUU' or 'UUC':
+# #         return 'Phenylalanine'
+# #     elif codon == 'UUA' or 'UUA' or 'CUU' or 'CUC' or 'CUA' or 'CUG':
+# #         return = 'Leucine'
+# #     elif codon == 'AUU' or 'AUC' or 'AUA':
+# #         return = 'Isoleucine'
+# #     elif codon == 'GUU' or 'GUC' or 'GUA' or 'GUG':
+# #         return ='Valine'
+# #     elif codon == 'UCU' or 'UCC' or 'UCA' or 'UCG':
+# #         return = 'Serine'
+# #     elif codon ==  'CCU' or 'CCC' or 'CCA' or 'CCG':
+# #         return = 'Proline'
+# #     elif codon == 'ACU' or 'ACC' or 'ACA' or 'ACG':
+# #         return = 'Threonine'
+# #     elif codon == 'GCU' or 'GCC' or 'GCA' or 'GCG':
+# #         return = 'Alanine'
+# #     elif codon == 'UAU' or 'UAC':
+# #         return = 'Tyrosine'
+# #     elif codon == 'CAU' or 'CAC':
+# #         return = 'Histidine'
+# #     elif codon == 'CAA' or 'CAG':
+# #         return = 'Glutamine'
+# #     elif codon == 'AAU' or 'AAC':
+# #         return = 'Lysine'
+# #     elif codon == 'GAU' or 'GAC':
+# #         return = 'Glutamate'
+# #     elif codon == 'UGU' or 'UGC':
+# #         return = 'Cysteine'
+# #     elif codon == 'UGG':
+# #         return = 'Tryptophan'
+# #     elif codon == 'CGU' or 'CGC' or ' CGA' or 'CGG' or 'AGA' or 'AGG':
+# #         return = 'Arginine'
+# #     elif codon == 'AGU' or 'AGC':
+# #         return = 'Serine'
+# #     elif codon == 'GGU' or 'GGC' or 'GGA' or 'GGG':
+# #         return = 'Glycine'
+# #     else:
+# #         return 'Stop codon'
 # =============================================================================
     
-print (33)
