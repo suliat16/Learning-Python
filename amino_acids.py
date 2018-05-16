43219033aace1881cbf01b825dea4c811ff873fc
suliat16 <suliat16@gmail.com>
2018-05-16 15:11:07 +0300

    added an amino acid to codon dictionary

--- amino_acids.py
+++ amino_acids.py
@@ -58,10 +58,14 @@ def codon_dict():
     ct['CAC']= 'Histidine'
     ct['CAA']= 'Glutamine'
     ct['CAG']= 'Glutamine'
-    ct['AAU']= 'Lysine'
-    ct['AAC']= 'Lysine'
-    ct['GAU']= 'Glutamate'
-    ct['GAC']= 'Glutamate'
+    ct['AAU']= 'Asparagine'
+    ct['AAC']= 'Asparagine'
+    ct['AAA']= 'Lysine'
+    ct['AAG']= 'Lysine'
+    ct['GAA']= 'Glutamate'
+    ct['GAG']= 'Glutamate'
+    ct['GAU']= 'Aspartate'
+    ct['GAC']= 'Aspartate'
     ct['UGU']= 'Cysteine'
     ct['UGC']= 'Cysteine'
     ct['UGG']= 'Tryptophan'
@@ -79,10 +83,41 @@ def codon_dict():
     ct['GGG']= 'Glycine'
     ct['UAA']= 'Stop codon'
     ct['UAG']= 'Stop codon'
-    ct['UGA'] = 'Stop codon'
+    ct['UGA']= 'Stop codon'
     
     return ct
 
+def amino_dict():
+    ''' A dictionary relating amino acids to potential codons
+    Args:
+    
+    Returns: A dictionary mapping each amino acid to the codons that encode for it
+    '''
+    at = dict()
+    at[('alanine', 'a', 'ala')]= ['GCC', 'GCA', 'GCU', 'GCG']
+    at[('arginine', 'arg', 'r')]= ['CGC', 'CGU', 'CGA', 'CGG', 'AGA', 'AGG']
+    at[('asparagine', 'asn', 'n')]= ['AAU', 'AAC']
+    at[('aspartate', 'aspartic acid', 'asp', 'd')]=['GAU', 'GAC']
+    at[('cysteine', 'cys', 'c')]=['UGU', 'UGC']
+    at[('glutamine', 'gln', 'e')]=['CAA', 'CAG']
+    at[('glutamate', 'glutamic acid', 'glu', 'q')]= ['GAA', 'GAG']
+    at[('glycine', 'gly', 'g')]= ['GGU', 'GGC', 'GGA', 'GGG']
+    at[('histidine', 'his', 'h')]=['CAU', 'CAC']
+    at[('isoleucine', 'ile', 'i')]= ['AUU', 'AUC', 'AUA']
+    at[('leucine', 'leu', 'l')]=['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG']
+    at[('lysine', 'lys', 'k')]= ['AAC', 'AAG'] 
+    at[('methionine', 'met', 'm')]=['AUG']
+    at[('phenylalanine', 'phe', 'f')]= ['UUU', 'UUC']
+    at[('proline', 'pro', 'p')]= ['CCU', 'CCC', 'CCA', 'CCG'] 
+    at[('serine', 'ser', 's')]= ['AGU', 'AGC']
+    at[('threonine', 'thr', 't')]= ['ACU', 'ACC', 'ACA', 'ACG']
+    at[('tryptophan', 'trp', 'w')]=['UGG']
+    at[('tyrosine', 'tyr', 'y')]= ['UAU', 'UAC'] 
+    at[('valine', 'val', 'v')]= ['GUU', 'GUC', 'GUA', 'GUG']
+   
+    return at
+    
+    
 def codon_to_aa(codon = 'AUG'):
     '''Takes a codon and returns an amino acid.
 
@@ -98,9 +133,19 @@ def codon_to_aa(codon = 'AUG'):
     '''
     cdict = codon_dict()
     codon = codon.upper()
-    assert(codon in cdict), "That isn't a codon, please try again."
+    assert(codon in cdict.keys()), "That isn't a codon, please try again."
     
     return cdict[codon]
+
+def aa_to_codon(aa= 'W'):
+    '''Takes an amino acid and returns the codons encoding for it
+    
+    Args:
+        aa: A string either containing the amino acid name, its 3 letter code, or 
+            its 1 letter code
+    Returns:
+        A list of the codons that could have coded for the amino acid
+    '''
     
 # =============================================================================
 # #     if codon == 'AUG':
