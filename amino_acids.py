4c47d0820841bc6f90105b54361c421ff40ef38e
suliat16 <suliat16@gmail.com>
2018-05-16 18:06:09 +0300

    Added error message in the case a non-amino acid is entered

--- amino_acids.py
+++ amino_acids.py
@@ -122,7 +122,7 @@ def codon_to_aa(codon = 'AUG'):
     '''Takes a codon and returns an amino acid.
 
     Args: 
-        codon: A string containing three letter. These letters can be ACUG, all caps,
+        codon (str): A string containing three letter. These letters can be ACUG, all caps,
             in any order or number, and each triplet corresponds to an amino acid
         
     Returns: The amino acid associated with the entered codon. 
@@ -151,52 +151,10 @@ def aa_to_codon(aa= 'W'):
     aa = aa.lower()
     akeys = adict.keys()
     
-    
+   
     for x in akeys:
         if aa in x:
             return adict[x]
     
-    
-# =============================================================================
-# #     if codon == 'AUG':
-# #         return 'Methionine'
-# #  #   elif codon == 'UUU' or 'UUC':
-# #         return 'Phenylalanine'
-# #     elif codon == 'UUA' or 'UUA' or 'CUU' or 'CUC' or 'CUA' or 'CUG':
-# #         return = 'Leucine'
-# #     elif codon == 'AUU' or 'AUC' or 'AUA':
-# #         return = 'Isoleucine'
-# #     elif codon == 'GUU' or 'GUC' or 'GUA' or 'GUG':
-# #         return ='Valine'
-# #     elif codon == 'UCU' or 'UCC' or 'UCA' or 'UCG':
-# #         return = 'Serine'
-# #     elif codon ==  'CCU' or 'CCC' or 'CCA' or 'CCG':
-# #         return = 'Proline'
-# #     elif codon == 'ACU' or 'ACC' or 'ACA' or 'ACG':
-# #         return = 'Threonine'
-# #     elif codon == 'GCU' or 'GCC' or 'GCA' or 'GCG':
-# #         return = 'Alanine'
-# #     elif codon == 'UAU' or 'UAC':
-# #         return = 'Tyrosine'
-# #     elif codon == 'CAU' or 'CAC':
-# #         return = 'Histidine'
-# #     elif codon == 'CAA' or 'CAG':
-# #         return = 'Glutamine'
-# #     elif codon == 'AAU' or 'AAC':
-# #         return = 'Lysine'
-# #     elif codon == 'GAU' or 'GAC':
-# #         return = 'Glutamate'
-# #     elif codon == 'UGU' or 'UGC':
-# #         return = 'Cysteine'
-# #     elif codon == 'UGG':
-# #         return = 'Tryptophan'
-# #     elif codon == 'CGU' or 'CGC' or ' CGA' or 'CGG' or 'AGA' or 'AGG':
-# #         return = 'Arginine'
-# #     elif codon == 'AGU' or 'AGC':
-# #         return = 'Serine'
-# #     elif codon == 'GGU' or 'GGC' or 'GGA' or 'GGG':
-# #         return = 'Glycine'
-# #     else:
-# #         return 'Stop codon'
-# =============================================================================
-    
+    return "That isn't an amino acid, please try again"
+  
