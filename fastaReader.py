555db09e29fa430c50dfdcc3f55a9efba63d3754
suliat16 <suliat16@gmail.com>
2018-05-21 10:03:26 +0300

    Changed the structure of the class, so that it could intake a fasta file with multiple (or a single) protein, and output the sequence

--- fastaReader.py
+++ fastaReader.py
@@ -8,7 +8,9 @@ Created on Sun May 20 10:06:57 2018
 Reads a FASTA file, returns a single string.
 """
 
-class FastaRead:
+import re
+
+class FastaRead():
     
     """
     Open FASTA file and return a stream
@@ -23,6 +25,8 @@ class FastaRead:
         """
         self.file=fasta
         self.object=open(self.file, 'r')
+        self.read = self.object.read()
+        
         
         
     def change_mode(self, mode='r'):
@@ -31,12 +35,29 @@ class FastaRead:
         self.object = open(self.file, mode)
         
         
-    def get_sequence(self):
+    def indv_block(self, st= ""):
         """
         """
-        strr=self.object.read()
+        fstr=re.split('(>)', st)
+        fstr = list(filter(None, fstr))
+        return fstr
+    
+    def get_head(self, index=0):
+        """
+        """
+        fstr= indv_block(self.read)
+        fstr=fstr[index]
+        fstr=re.split('>|\s', fstr)
+        return fstr[0]
+    
+    def get_sequence(self, index=0):
+        """
+        """
+        fstr=indv_block(self.read)
+        fstr=fstr[index]
+        fstr= fstr.splitlines()
+        for f in fstr:
+            if f.startswith(">"):
+                fstr.remove(f)
+        fstr= "".join(fstr)
         
-        
-
-
-
--- fastaTest.py
+++ fastaTest.py
@@ -9,6 +9,33 @@ Test cases
 """
 
 import unittest
+#import fastaReader
 
 class TestFasta(unittest.TestCase):
+    def setUp(self):
+        self.fasta=FastaRead('1dpx.fasta.txt')
+        self.titin=FastaRead('titin.fasta')
+        self.multi=FastaRead('multiFasta.fasta')
+        
+    
+    def test_get_sequence_lysozyme(self):
+        self.assertEqual(self.fasta.get_sequence(),'KVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGILQINSRWWCNDGRTPGSRNLCNIPCSALLSSDITASVNCAKKIVSDGNGMNAWVAWRNRCKGTDVQAWIRGCRL')
+    
+    def test_get_sequence_multi_v2(self):
+        self.assertEqual(self.multi.get_sequence(index=2), 'TTGGCCAA')
+        
+    def test_indv_block(self):
+        self.assertEqual(len(self.multi.indv_block()), 3)
+    
+    def test_indv_block_sin(self):
+        self.assertEqual(self.fasta.indv_block(), ['>1DPX:A|PDBID|CHAIN|SEQUENCE\nKVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGILQINSRWWCNDGRTPGSRNLCNIPC\nSALLSSDITASVNCAKKIVSDGNGMNAWVAWRNRCKGTDVQAWIRGCRL'])
+        
+    def test_head(self):
+        self.assertEqual(self.multi.get_head(), ['AT1G01140.1', 'AT1G01140.1', 'AT1G01140.2'])
+        
+    def test_head_single(self):
+        self.assertEqual(self.titin.get_head(), ['CAA62188.1'])
+        
+    
+    
     
